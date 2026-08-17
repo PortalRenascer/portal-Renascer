b0b26412ecdc07670df0dc7a435c395d3505d9e5
PortalRenascer <renascer.ca2023@gmail.com>
2026-08-17 11:00:18 -0300

    acerto erro de minuta

--- app.py
+++ app.py
@@ -138,8 +138,11 @@ elif opcao == "Cadastrar Minutas":
                             "nf": nf_limpa,
                             "url_foto": url_foto
                         }
+                    try:
                         supabase.table("minutas").insert(dados).execute()
                         st.success("✅ Minuta cadastrada com sucesso! Os campos foram limpos para o próximo cadastro.")
+                    except Exception as e:
+                        st.error(f"Erro do Supabase: {e}")
                     else:
                         st.error("Erro ao enviar a imagem. Tente novamente.")
         else:
